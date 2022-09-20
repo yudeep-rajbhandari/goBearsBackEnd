eae989c1203f304ebd3ef5f66fdfde91174c3dfb
Nises <nishandhojkarki@gmail.com>
2022-09-20 18:34:22 -0500

    Added the resource and resource reservation entity.

--- src/main/java/com/se/goBears/entity/Resource.java
+++ src/main/java/com/se/goBears/entity/Resource.java
@@ -4,7 +4,34 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-
+@Data
+@Entity
 public class Resource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    private String name;
+    private boolean availability;
+    private WorkingCondition workingCondition;
+    private ResourceType resourceType;
+    @ManyToOne
+    private ResourceReservation resourceReservation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private enum WorkingCondition{
+        Good,Excellent,Old
+    }
+
+    private enum ResourceType{
+        Indoor,Outdoor
+    }
 }
--- src/main/java/com/se/goBears/entity/ResourceReservation.java
+++ src/main/java/com/se/goBears/entity/ResourceReservation.java
@@ -3,11 +3,25 @@ package com.se.goBears.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.Set;
 
-
+@Data
+@Entity
 public class ResourceReservation extends Reservations{
+    @Id
+    private Long id;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @OneToMany
+    private Set<Resource> resource;
 }
