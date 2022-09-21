561344223cbc88ce6015eb3c61ff9e947053fa35
Mahee Noor Tayba <mahee.noor.tayba@gmail.com>
2022-09-20 19:37:31 -0500

    direction added

--- src/main/java/com/se/goBears/entity/Direction.java
+++ src/main/java/com/se/goBears/entity/Direction.java
@@ -4,7 +4,24 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-
+@Data
+@Entity
 public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    private String description;
+
+    @OneToOne
+    private Room room;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
