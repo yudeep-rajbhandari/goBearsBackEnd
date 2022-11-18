8e427a8d286f65ada905e2ba17543ab8dc64e8d6
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-18 02:07:38 -0600

    cleanup all getter setter

--- src/main/java/com/se/goBears/entity/Schedule.java
+++ src/main/java/com/se/goBears/entity/Schedule.java
@@ -8,55 +8,20 @@ import java.util.Date;
 @Data
 @Entity
 public class Schedule {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
     private String name;
+
     private Date fromDate;
+
     private Date toDate;
 
     @ManyToOne
     private Room room;
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public Date getFromDate() {
-//        return fromDate;
-//    }
-//
-//    public void setFromDate(Date fromDate) {
-//        this.fromDate = fromDate;
-//    }
-//
-//    public Date getToDate() {
-//        return toDate;
-//    }
-//
-//    public void setToDate(Date toDate) {
-//        this.toDate = toDate;
-//    }
-//
-//    public Room getRoom() {
-//        return room;
-//    }
-//
-//    public void setRoom(Room room) {
-//        this.room = room;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+
 }
