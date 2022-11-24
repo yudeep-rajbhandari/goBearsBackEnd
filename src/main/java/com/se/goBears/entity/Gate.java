b9e77ba485ab51ff8eb0b16d39f87d06b8f7ae2b
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-11-24 02:00:36 -0600

    gate added

--- src/main/java/com/se/goBears/entity/Gate.java
+++ src/main/java/com/se/goBears/entity/Gate.java
@@ -3,30 +3,17 @@ package com.se.goBears.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @Entity
-public class Gate {
+public class Gate implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @GeneratedValue
     private Long id;
 
     private String name;
-
-    private Long latitude;
-
-    private long longitude;
-
-    private CardinalDirection cardinalDirection;
-
-    @OneToOne
-    private Building building;
-
-
-
-    private enum CardinalDirection{
-        North,South,East,West
-    }
+    private Double latitude;
+    private Double longitude;
 }
