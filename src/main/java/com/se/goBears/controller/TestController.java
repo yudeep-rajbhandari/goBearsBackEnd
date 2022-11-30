c7c6382d08e73d56821b0ab32910b0d8e80c00a4
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-11-30 09:21:59 -0600

    refactor

--- src/main/java/com/se/goBears/controller/TestController.java
+++ src/main/java/com/se/goBears/controller/TestController.java
@@ -5,7 +5,10 @@ import com.se.goBears.entity.User;
 import com.se.goBears.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -15,6 +18,7 @@ public class TestController {
 
     @Autowired
     UserService userService;
+
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
@@ -41,12 +45,9 @@ public class TestController {
 
     @GetMapping("/findUserByEmail/{email}")
     @PreAuthorize("hasRole('USER')")
-    public User findUserByEmail(String email){
+    public User findUserByEmail(String email) {
         return userService.findUserByEmail(email);
     }
-//    @PutMapping("/updateRole")
-//    public User updateRole(@PathVariable Long userId){
-//        return userService.findUserByEmail()
-//    }
+
 }
 
