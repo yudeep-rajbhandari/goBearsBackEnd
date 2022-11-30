13c41432c1f1ca23d2d51d78b1e3b3386596e310
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-11-29 23:15:32 -0600

    javadoc

--- src/main/java/com/se/goBears/controller/AllotmentController.java
+++ src/main/java/com/se/goBears/controller/AllotmentController.java
@@ -15,7 +15,8 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 
 /**
- * <p>This controller handles all the API requests for Allotment</p>
+ * <p>This controller class handles all the API requests for Allotment. It handles the request to add an allotment,
+ * get a list of all allotments and get a list of allotment for a specific user.</p>
  * @param
  * @return
  * @see
@@ -27,17 +28,31 @@ public class AllotmentController {
     @Autowired
     private AllotmentService allotmentService;
 
+    /**
+     * This method adds an allotment record.
+     * @param allotment A request-body for an allotment.
+     * @return The saved allotment.
+     */
     @PostMapping("addAllotment")
     public Allotment addAllotment(@RequestBody Allotment allotment)  {
         return allotmentService.addAllotment(allotment);
     }
 
+    /**
+     * This method gets all allotment.
+     * @param
+     * @return List of all allotments.
+     */
     @GetMapping("/getAllAllotment")
     public List<Allotment> getAllAllotment(){
         return allotmentService.getAllAllotment();
     }
 
-
+    /**
+     * This method an allotment for a user.
+     * @param userId The userid for the allotment.
+     * @return List of the allotment for a user.
+     */
     @GetMapping("/getMyAllotment/{userId}")
     public List<Allotment> getMyAllotment(@PathVariable Long userId){
         return allotmentService.getMyAllotment(userId);
