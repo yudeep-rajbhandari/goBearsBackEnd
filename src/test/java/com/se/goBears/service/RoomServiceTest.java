dddde1a91e8bdd503bff53ae242cc998d21cb8be
yudeep-rajbhandari <aezakmi@12345678>
2022-11-30 12:53:09 -0600

    unit test for room

--- src/test/java/com/se/goBears/service/RoomServiceTest.java
+++ src/test/java/com/se/goBears/service/RoomServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 public class RoomServiceTest {
 
@@ -19,6 +21,20 @@ public class RoomServiceTest {
         room.setIsBookable(true);
         room.setRoomType(Room.RoomType.CLASSROOM);
         Room room1 = roomService.addRoom(room);
+        roomService.deleteroomById(room1.getId());
+
+    }
+
+    @Test
+    public void sameRoomErrorTest() throws Exception {
+        Room room = new Room();
+        room.setName("Test Room");
+        room.setIsBookable(true);
+        room.setRoomType(Room.RoomType.CLASSROOM);
+        Room room1 = roomService.addRoom(room);
+        Exception exception = assertThrows(Exception.class, () -> {
+            roomService.addRoom(room1);
+        });
         roomService.deleteroomById(room1.getId());
 
     }
