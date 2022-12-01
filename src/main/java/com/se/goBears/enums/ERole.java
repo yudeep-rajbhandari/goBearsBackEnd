45db4fb15e39e169ccd7acbef8a3d3c858695c6c
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 14:10:42 -0600

    Java Doc

--- src/main/java/com/se/goBears/enums/ERole.java
+++ src/main/java/com/se/goBears/enums/ERole.java
@@ -1,6 +1,9 @@
 package com.se.goBears.enums;
 
+
+/**
+ * This is the enum class for the user role defined as Admin or User.
+ */
 public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER, ROLE_ADMIN
 }
