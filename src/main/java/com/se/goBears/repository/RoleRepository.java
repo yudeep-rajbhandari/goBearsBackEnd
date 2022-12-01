cd18bdee0dfc0584506c5bf7f0c9832e018df968
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 16:09:36 -0600

    Java Doc

--- src/main/java/com/se/goBears/repository/RoleRepository.java
+++ src/main/java/com/se/goBears/repository/RoleRepository.java
@@ -8,7 +8,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * This is a repository interface for Role entity.
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    /**
+     * This method returns the role object given the role name.
+     * @param name is the name of the role.
+     * @return role associated with the name.
+     */
     Optional<Role> findByName(ERole name);
 }
