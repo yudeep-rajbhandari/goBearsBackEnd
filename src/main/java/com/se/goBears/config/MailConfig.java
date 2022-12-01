3ac95fba271a093ea8145852f15217d71b466ca0
yudeep-rajbhandari <endlessveins@gmail.com>
2022-12-01 14:36:13 -0600

    mailService confirmed for all mail setup increased

--- src/main/java/com/se/goBears/config/MailConfig.java
+++ src/main/java/com/se/goBears/config/MailConfig.java
@@ -10,21 +10,21 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
-//    @Bean
-//    public JavaMailSender sendEmail(){
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.gmail.com");
-//        mailSender.setPort(587);
-//
-//        mailSender.setUsername("yudeep.rajbhandari@gmail.com");
-//        mailSender.setPassword("wutzfsbsnwxnhozq");
-//
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
-//
-//        return mailSender;
-//    }
+    @Bean
+    public JavaMailSender sendEmail(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("yudeep.rajbhandari@gmail.com");
+        mailSender.setPassword("wutzfsbsnwxnhozq");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 }
