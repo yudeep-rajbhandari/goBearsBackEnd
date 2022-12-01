1d8b663e51e9919837d7101d35acdebe14206006
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 15:50:59 -0600

    Java Doc

--- src/main/java/com/se/goBears/repository/BuildingRepository.java
+++ src/main/java/com/se/goBears/repository/BuildingRepository.java
@@ -8,11 +8,25 @@ import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * This is a repository interface for Building entity.
+ */
 @Repository
 @Transactional
 public interface BuildingRepository extends CrudRepository<Building, Long> {
+    /**
+     * This method returns the building object.
+     *
+     * @param buildingId is the id of the building to be found
+     * @return the building object.
+     */
     Building findBuildingById(Long buildingId);
 
+    /**
+     * This method returns a list of all buildings.
+     *
+     * @return a list of all the buildings.
+     */
     List<Building> findAll();
 
 }
