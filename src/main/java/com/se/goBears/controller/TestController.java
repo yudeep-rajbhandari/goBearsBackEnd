0ec098b004ef83ce74bdd23367e855dff7f41d56
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 11:52:20 -0600

    Java Doc

--- src/main/java/com/se/goBears/controller/TestController.java
+++ src/main/java/com/se/goBears/controller/TestController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * This is a test controller class that is used for testing.
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
@@ -15,30 +18,50 @@ public class TestController {
 
     @Autowired
     UserService userService;
+
+    /**
+     * This method tests if the system handle the public access.
+     * @return message on success
+     */
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
     }
 
+    /**
+     * This method check if the system handle the access as a user.
+     * @return message on success.
+     */
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER')")
     public String userAccess() {
         return "User Board.";
     }
 
+    /**
+     * This method check if the system handle the access as a admin.
+     * @return message on success.
+     */
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
     public String adminAccess() {
         return "Admin Board.";
     }
 
+    /**
+     * This method tests how the system handle admin access to adding a building.
+     * @return if user is admin a success message is displayed.
+     */
     @GetMapping("/building/addBuilding")
     @PreAuthorize("hasRole('ADMIN')")
     public String addBuilding() {
         return "Add Building here";
     }
 
-
+    /**
+     * This method tests how the system handle user email.
+     * @return if user email if exists.
+     */
     @GetMapping("/findUserByEmail/{email}")
     @PreAuthorize("hasRole('USER')")
     public User findUserByEmail(String email){
