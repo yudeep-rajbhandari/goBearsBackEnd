33493a8de37853c30f81b2495e9e31c8a807926b
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 16:22:18 -0600

    Java Doc

--- src/main/java/com/se/goBears/repository/ScheduleRepository.java
+++ src/main/java/com/se/goBears/repository/ScheduleRepository.java
@@ -6,9 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * This is a repository interface for Schedule entity.
+ */
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+    /**
+     * This method returns all schedule given a room id and a from date.
+     * @param roomId is the id of the room.
+     * @param fromdate is the from date for the schedule.
+     * @return a list of schedule for a room after the given from date.
+     */
     List<Schedule> findScheduleByRoom_IdAndFromDateGreaterThan(Long roomId, Date fromdate);
 
+    /**
+     * This method returns a schedule list given a from and a to date.
+     * @param fromdate is the date for the search to begin.
+     * @param toDate is the date for which search is to end.
+     * @return a list of schedule between the fron and to dates.
+     */
     List<Schedule> findScheduleByFromDateGreaterThanAndToDateLessThan(Date fromdate,Date toDate);
 }
