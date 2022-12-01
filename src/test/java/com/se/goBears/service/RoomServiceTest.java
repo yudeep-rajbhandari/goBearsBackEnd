d78be96832cb1c9601ce4abc31417494de87e4a0
yudeep-rajbhandari <endlessveins@gmail.com>
2022-12-01 14:34:39 -0600

    testcases ammended again

--- src/test/java/com/se/goBears/service/RoomServiceTest.java
+++ src/test/java/com/se/goBears/service/RoomServiceTest.java
@@ -28,44 +28,44 @@ public class RoomServiceTest {
 
     }
 
-//    @Test
-//    public void sameRoomErrorTest() throws Exception {
-//        Room room = new Room();
-//        room.setName("Test Room");
-//        room.setIsBookable(true);
-//        room.setRoomType(Room.RoomType.CLASSROOM);
-//        Room room1 = roomService.addRoom(room);
-//        Exception exception = assertThrows(Exception.class, () -> {
-//            roomService.addRoom(room1);
-//        });
-//        roomService.deleteroomById(room1.getId());
-//
-//    }
-//
-//    @Test
-//    public void updateRoomTest() throws Exception {
-//        Room room = new Room();
-//        room.setName("Test Room");
-//        room.setIsBookable(true);
-//        room.setRoomType(Room.RoomType.CLASSROOM);
-//        Room room1 = roomService.addRoom(room);
-//        room1.setName("Changed name");
-//        Room room2 = roomService.updateRoom(room1);
-//        assertEquals(room2.getName(),"Changed name");
-//        roomService.deleteroomById(room1.getId());
-//    }
-//
-//    @Test
-//    public void makeBookableFalseTest() throws Exception {
-//        Room room = new Room();
-//        room.setName("Test Room");
-//        room.setIsBookable(true);
-//        room.setRoomType(Room.RoomType.CLASSROOM);
-//        Room room1 = roomService.addRoom(room);
-//
-//        Room room2 = roomService.makeBookableFalse(room1);
-//        assertEquals(room2.isBookable(),false);
-//        roomService.deleteroomById(room1.getId());
-//
-//    }
+    @Test
+    public void sameRoomErrorTest() throws Exception {
+        Room room = new Room();
+        room.setName("Test Room");
+        room.setIsBookable(true);
+        room.setRoomType(Room.RoomType.CLASSROOM);
+        Room room1 = roomService.addRoom(room);
+        Exception exception = assertThrows(Exception.class, () -> {
+            roomService.addRoom(room1);
+        });
+        roomService.deleteroomById(room1.getId());
+
+    }
+
+    @Test
+    public void updateRoomTest() throws Exception {
+        Room room = new Room();
+        room.setName("Test Room");
+        room.setIsBookable(true);
+        room.setRoomType(Room.RoomType.CLASSROOM);
+        Room room1 = roomService.addRoom(room);
+        room1.setName("Changed name");
+        Room room2 = roomService.updateRoom(room1);
+        assertEquals(room2.getName(),"Changed name");
+        roomService.deleteroomById(room1.getId());
+    }
+
+    @Test
+    public void makeBookableFalseTest() throws Exception {
+        Room room = new Room();
+        room.setName("Test Room");
+        room.setIsBookable(true);
+        room.setRoomType(Room.RoomType.CLASSROOM);
+        Room room1 = roomService.addRoom(room);
+
+        Room room2 = roomService.makeBookableFalse(room1);
+        assertEquals(room2.isBookable(),false);
+        roomService.deleteroomById(room1.getId());
+
+    }
 }
