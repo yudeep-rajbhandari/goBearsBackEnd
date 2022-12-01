9f0ee5ff844031560cdced644f9a1caf272705f1
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 14:14:39 -0600

    Java Doc

--- src/main/java/com/se/goBears/errors/CustomException.java
+++ src/main/java/com/se/goBears/errors/CustomException.java
@@ -1,22 +1,46 @@
 package com.se.goBears.errors;
 
+
+/**
+ * This is a custom exception class generated to display error messages.
+ */
 public class CustomException extends RuntimeException {
+    /**
+     * This variable defines the message for the error.
+     */
     private String message;
 
+    /**
+     * This is a constructor for the class.
+     */
     public CustomException() {
     }
 
-    public CustomException(
-            String message) {
+    /**
+     * This is a constructor that sets the message value.
+     *
+     * @param message is the message for the error.
+     */
+    public CustomException(String message) {
         this.message = message;
 
     }
 
+    /**
+     * This method returns the error message.
+     *
+     * @return error message.
+     */
     @Override
     public String getMessage() {
         return message;
     }
 
+    /**
+     * This method sets the error message.
+     *
+     * @param message is the error mesage to be set.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
