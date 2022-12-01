125b2ef0f069ff0d790ef6b12c26f27f1953be25
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 17:32:16 -0600

    Java Doc

--- src/main/java/com/se/goBears/service/ReservationService.java
+++ src/main/java/com/se/goBears/service/ReservationService.java
@@ -18,19 +18,21 @@ public class ReservationService {
 
     /**
      * This method returns a list of reservations associated with a user id.
+     *
      * @param userId is the id of the user to be looked for.
      * @return a list of reservation associated with a user.
      */
-    public List<Reservations> getMyReservation(Long userId){
+    public List<Reservations> getMyReservation(Long userId) {
         return reservationRepository.findReservationsByBookedBy(Math.toIntExact(userId));
     }
 
     /**
      * This method given the reservation id, cancels the reservation.
+     *
      * @param id is the id of the reservation to be cancelled.
      * @return the reservation object with cancelled status.
      */
-    public Reservations cancelReservation(Long id){
+    public Reservations cancelReservation(Long id) {
         Reservations reservations = reservationRepository.findReservationsById(id);
         reservations.setStatus(Reservations.Status.CANCELED);
 
