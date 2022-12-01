d418cebfbad5bb81c53d16b017d49f5a93b8c5cf
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 15:43:41 -0600

    Java Doc

--- src/main/java/com/se/goBears/repository/AddressRepository.java
+++ src/main/java/com/se/goBears/repository/AddressRepository.java
@@ -5,8 +5,18 @@ import com.se.goBears.entity.Building;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long>  {
 
+/**
+ * This is a repository interface for Address entity.
+ */
+@Repository
+public interface AddressRepository extends CrudRepository<Address, Long> {
+
+    /**
+     * On an input id the method returns the address associated with the id.
+     *
+     * @param id is the id to be found.
+     * @return an address object.
+     */
     Address findAddressById(Long id);
 }
