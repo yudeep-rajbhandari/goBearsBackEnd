d78ed3f28164b977a4af96a9f3bfe484f42491b9
yudeep-rajbhandari <endlessveins@gmail.com>
2022-12-01 14:35:56 -0600

    mailService confirmed for all mail setup

---
+++ src/main/java/com/se/goBears/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.se.goBears.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+//    @Bean
+//    public JavaMailSender sendEmail(){
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("yudeep.rajbhandari@gmail.com");
+//        mailSender.setPassword("wutzfsbsnwxnhozq");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
+}
