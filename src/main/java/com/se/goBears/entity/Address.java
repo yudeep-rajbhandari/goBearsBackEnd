28361421bd878c31c35c58393b7b97b6e1a07103
Nishan Karki <112372298+NishanDkarki@users.noreply.github.com>
2022-12-01 12:12:11 -0600

    Java Doc

--- src/main/java/com/se/goBears/entity/Address.java
+++ src/main/java/com/se/goBears/entity/Address.java
@@ -16,10 +16,25 @@ import java.io.Serializable;
 public class Address implements Serializable {
     @Id
     @GeneratedValue
+    /**
+     * This is the auto generated id for the class objects.
+     */
     private Long id;
+    /**
+     * This variable define the street name for the address.
+     */
     private String street;
+    /**
+     * This variable define the city name for the address.
+     */
     private String city;
+    /**
+     * This variable define the state name for the address.
+     */
     private String state;
+    /**
+     * This variable define the zip code for the address.
+     */
     private String zip;
 
 
