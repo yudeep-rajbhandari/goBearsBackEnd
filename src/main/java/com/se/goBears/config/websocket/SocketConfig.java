0d873778ff5b7cb51e73a7c27a30006e748cccbb
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-12-05 03:51:59 -0600

    websocket fix

--- src/main/java/com/se/goBears/config/websocket/SocketConfig.java
+++ src/main/java/com/se/goBears/config/websocket/SocketConfig.java
@@ -28,7 +28,7 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/myws").setAllowedOrigins("*").setHandshakeHandler(new DefaultHandshakeHandler() {
+        registry.addEndpoint("/myws").setAllowedOriginPatterns("*").setHandshakeHandler(new DefaultHandshakeHandler() {
 
             @Override
             protected Principal determineUser(ServerHttpRequest request,
