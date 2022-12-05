8e29ca993d539ee4b655ad46889c73bc18b04b87
yudeep-rajbhandari <endlessveins@gmail.com>
2022-12-04 22:12:29 -0600

    ReservationServiceTest updated

--- src/test/java/com/se/goBears/service/ReservationServiceTest.java
+++ src/test/java/com/se/goBears/service/ReservationServiceTest.java
@@ -1,6 +1,7 @@
 package com.se.goBears.service;
 
 import com.se.goBears.entity.Reservations;
+import com.se.goBears.entity.Room;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,30 +16,45 @@ public class ReservationServiceTest {
     private RoomReservationService reservationService;
 
     @Autowired
+    private RoomService roomService;
+
+    @Autowired
     private  ResourceReservationService resourceReservationService;
 
     @Test
-    public void reserveRoomTest(){
+    public void reserveRoomTest() throws Exception {
+        Room room = new Room();
+        room.setName("Test Room");
+        room.setIsBookable(true);
+        room.setRoomType(Room.RoomType.CLASSROOM);
+        Room room1 = roomService.addRoom(room);
         Reservations reservations = new Reservations();
         reservations.setFromDate(new Date().toString());
         reservations.setToDate(new Date().toString());
 
-        Reservations reservations1 = reservationService.saveRoomReservation(reservations,Long.valueOf(23));
+        Reservations reservations1 = reservationService.saveRoomReservation(reservations, room1.getId());
         Assertions.assertEquals(reservations1.getReserveType(), Reservations.ReserveType.ROOM);
         reservationService.deleteReservationByid(reservations1.getId());
+        roomService.deleteroomById(room1.getId());
 
     }
 
-    @Test
-    public void reserveResourceTest(){
-        Reservations reservations = new Reservations();
-        reservations.setFromDate(new Date().toString());
-        reservations.setToDate(new Date().toString());
-
-        Reservations reservations1 = resourceReservationService.saveRoomReservation(reservations,Long.valueOf(23));
-        Assertions.assertEquals(reservations1.getReserveType(), Reservations.ReserveType.RESOURCE);
-        reservationService.deleteReservationByid(reservations1.getId());
-
-    }
+//    @Test
+//    public void reserveResourceTest() throws Exception {
+//        Room room = new Room();
+//        room.setName("Test Room");
+//        room.setIsBookable(true);
+//        room.setRoomType(Room.RoomType.CLASSROOM);
+//        Room room1 = roomService.addRoom(room);
+//        Reservations reservations = new Reservations();
+//        reservations.setFromDate(new Date().toString());
+//        reservations.setToDate(new Date().toString());
+//
+//        Reservations reservations1 = resourceReservationService.saveRoomReservation(reservations,room1.getId());
+//        Assertions.assertEquals(reservations1.getReserveType(), Reservations.ReserveType.RESOURCE);
+//        reservationService.deleteReservationByid(reservations1.getId());
+//        roomService.deleteroomById(room1.getId());
+//
+//    }
 
 }
